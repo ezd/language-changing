82c21920f358b37652828f9a8c893bcc1cd1e375
Ezedin <biliyala.ezd2@gmail.com>
2017-10-26 12:27:45 -0500

    Logger experiment

--- experiment/src/main/java/com/example/demo/IndexController.java
+++ experiment/src/main/java/com/example/demo/IndexController.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-
+private final Logger LOGGER=LoggerFactory.getLogger(IndexController.class);
 	@RequestMapping("/")
 	public String getIndex(){
+		LOGGER.debug("This is a debug message");
+	    LOGGER.info("This is an info message");
+	    LOGGER.warn("This is a warn message");
+	    LOGGER.error("This is an error message");
 		return "index";
 	}
 }
